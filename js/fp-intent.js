f82371d1e9ba6ab4537d6a01479b0cac76d592fe
Aidan Warner <aidan@awarner.co.uk>
2023-03-10 19:48:16 +0000

    Update fp anchors

--- js/fp-intent.js
+++ js/fp-intent.js
@@ -1,8 +1,7 @@
 var myFullPage = new fullpage('#fullpage', {
-    anchors:['slidehome','slidefeatures','slidescreenshots','slideteam','slidedownloads','slideget-involved'],
+    anchors:['home','features','screenshots','team','downloads','get-involved'],
     licenseKey:'gplv3-license',
     navigation:true,
     navigationPosition:'right',
     navigationTooltips:['Home','Features','Screenshots','Meet Our Team','Downloads','Get Involved','Donate'],
 });
-
