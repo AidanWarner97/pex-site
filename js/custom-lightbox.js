bbd899b9be01f00d881e19be2023c43868d56c82
Aidan Warner <aidan@awarner.co.uk>
2023-03-10 19:48:46 +0000

    Add lightbox JS
    Also include custom icons for lightbox views

---
+++ js/custom-lightbox.js
@@ -0,0 +1,6 @@
+function insertIcons() {
+    document.getElementsByClassName('lightbox-btn-previous').innerHTML = '<i class="fa-solid fa-arrow-left"></i>';
+    document.getElementsByClassName('lightbox-btn-next').innerHTML = '<i class="fa-solid fa-arrow-right"></i>';
+};
+
+insertIcons();
